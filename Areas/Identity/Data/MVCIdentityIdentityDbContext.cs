6187cd49605ffd980aed6706dd20a2cef6b599f0
nevercost <onanibosibabajide@gmail.com>
2020-03-21 12:48:33 +0100

    initial commit

---
+++ Areas/Identity/Data/MVCIdentityIdentityDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCIdentity.Areas.Identity.Data
+{
+    public class MVCIdentityIdentityDbContext : IdentityDbContext<IdentityUser>
+    {
+        public MVCIdentityIdentityDbContext(DbContextOptions<MVCIdentityIdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
